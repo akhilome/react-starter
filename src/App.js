4c81bd468236ed80422551175d8224d18e99d319
Kizito Akhilome <hovkard@gmail.com>
2019-01-06 09:04:27 +0100

    configure babel with webpack for react
    - install standard react libs (react, react-dom, prop-types)
    - install @babel/preset-react
    - add new preset to babel-loader options in webpack.config.js

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <div>Hello!! 👋🏾</div>;
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-const name = 'down';
-const f = () => console.log(`man ${name}`);
-f();
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.querySelector('#root'));
