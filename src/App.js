2440873db2307a3f3585f32c010f428ebdf9e345
Kizito Akhilome <hovkard@gmail.com>
2019-01-08 13:09:31 +0100

    configure babel to use js class properties plugin
    - install @babel/plugin-proposal-class-properties
    - modify babel loader to use plugin
    - add counter to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import './index.css';
+import React, { Component } from 'react';
 
-const App = () => {
-  return <div>Hello!! 👋🏾</div>;
-};
+class App extends Component {
+  state = { count: 0 };
+
+  increment = () => {
+    this.setState(prevState => ({ count: prevState.count + 1 }));
+  };
+
+  decrement = () => {
+    this.setState(prevState => ({ count: prevState.count - 1 }));
+  };
+  render() {
+    return (
+      <div>
+        <h2>Counter</h2>
+        <p>Number: {this.state.count}</p>
+        <button onClick={this.increment}>+1</button>
+        <button onClick={this.decrement}>-1</button>
+      </div>
+    );
+  }
+}
 
 export default App;
